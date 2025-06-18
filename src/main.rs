0d32267a4eddd483c421a8ae2fd073b0ad3d245e
David Long <david.long@codeglacier.ca>
2025-06-18 17:14:34 -0400

    feat: open and save image

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 use clap::Parser;
 use env_logger::Env;
+use image::DynamicImage;
 use log::info;
 
 use crate::args::RuntimeArgs;
 
 mod args;
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: RuntimeArgs = RuntimeArgs::parse();
 
     env_logger::builder()
@@ -19,4 +20,17 @@ fn main() {
         env!("CARGO_PKG_NAME"),
         env!("CARGO_PKG_VERSION")
     );
+
+    // Open image file
+    let image: DynamicImage = image::open(args.input)?;
+
+    // Save processed image
+    let processed_image = process_image(&image)?;
+    processed_image.save(args.output)?;
+
+    Ok(())
+}
+
+fn process_image(image: &DynamicImage) -> Result<DynamicImage, Box<dyn std::error::Error>> {
+    Ok(image.clone())
 }
